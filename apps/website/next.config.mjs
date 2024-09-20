f39a4e2ce57485f18ca7851df36d84fd852034c1
Jean-Paul Ainam <jpainam@gmail.com>
2024-09-20 17:16:29 -0400

    adding website

---
+++ apps/website/next.config.mjs
@@ -0,0 +1,36 @@
+/** @type {import("next").NextConfig} */
+const config = {
+  poweredByHeader: false,
+  reactStrictMode: true,
+  transpilePackages: ["@repo/ui"],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/en/(.*)",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/public-beta",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
+
+export default config;
