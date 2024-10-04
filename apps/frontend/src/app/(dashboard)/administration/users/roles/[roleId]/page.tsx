38605359ab13dc4e8f21fbed26446582102d07c3
Jean-Paul Ainam <jpainam@gmail.com>
2024-10-04 07:29:27 -0400

    Refactor page component for role administration
    - Removed unused import of Label component
    - Added PageHeader component for consistent page layout
    - Updated Tabs component to include className prop

--- apps/frontend/src/app/(dashboard)/administration/users/roles/[roleId]/page.tsx
+++ apps/frontend/src/app/(dashboard)/administration/users/roles/[roleId]/page.tsx
@@ -1,10 +1,10 @@
 import { notFound } from "next/navigation";
 
 import { getServerTranslations } from "@repo/i18n/server";
-import { Label } from "@repo/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@repo/ui/tabs";
 
 import { api } from "~/trpc/server";
+import { PageHeader } from "../../../PageHeader";
 import { PolicyDataTable } from "./PolicyDataTable";
 import { UserDataTable } from "./UserDataTable";
 
@@ -13,31 +13,17 @@ export default async function Page({
 }: {
   params: { roleId: string };
 }) {
-  const { t, i18n } = await getServerTranslations();
+  const { t } = await getServerTranslations();
   const role = await api.role.get(roleId);
-  const dateFormatter = Intl.DateTimeFormat(i18n.language, {
-    day: "numeric",
-    month: "short",
-    year: "numeric",
-  });
+
   if (!role) {
     notFound();
   }
   return (
-    <div className="flex flex-col gap-4 p-2">
-      <div className="grid divide-y rounded-lg border bg-secondary px-4 text-secondary-foreground md:grid-cols-3">
-        <div className="flex flex-row items-center gap-2">
-          <Label>
-            {t("role")} - {t("name")}
-          </Label>
-          <span>{role.name}</span>
-        </div>
-        <div className="flex flex-row items-center gap-2">
-          <Label>{t("createdAt")}</Label>
-          <span>{dateFormatter.format(role.createdAt)}</span>
-        </div>
-      </div>
-      <Tabs defaultValue="users">
+    <div className="flex flex-col gap-2">
+      <PageHeader title={`${t("role")} - ${role.name}`} />
+
+      <Tabs defaultValue="users" className="px-2">
         <TabsList>
           <TabsTrigger value="users">{t("users")}</TabsTrigger>
           <TabsTrigger value="permissions">{t("permissions")}</TabsTrigger>
