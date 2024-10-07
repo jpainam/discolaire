0df6b99a70b65c705fdbb57addf1e2897ce02b3f
Jean-Paul Ainam <jpainam@gmail.com>
2024-10-06 22:45:42 -0400

    Refactor types: Remove unused type definitions

--- apps/frontend/src/types/classroom_level.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const classroomLevelSchema = z.object({
-  id: z.number(),
-  name: z.string(),
-});
-
-export const classroomLevelsSchema = z.array(classroomLevelSchema);
-export type ClassroomLevel = z.infer<typeof classroomLevelSchema>;
--- apps/frontend/src/types/country.ts
+++
@@ -1,9 +0,0 @@
-// import { z } from "zod";
-
-// export const countrySchema = z.object({
-//   id: z.string(),
-//   name: z.string(),
-//   codeIso3: z.string().nullish(),
-// });
-
-// export type Country = z.infer<typeof countrySchema>;
