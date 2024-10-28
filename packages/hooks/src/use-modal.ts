e7ff2f2e8e79f6f2828147f19d3155afee02e3f4
Jean-Paul Ainam <jpainam@gmail.com>
2024-10-27 21:57:24 -0400

    Update dependencies to @turbo/gen and turbo to version 2.1.2

--- packages/hooks/src/use-modal.ts
+++ packages/hooks/src/use-modal.ts
@@ -1,6 +1,7 @@
 "use client";
 
 import type React from "react";
+import { useCallback } from "react";
 import { atom, useAtomValue, useSetAtom } from "jotai";
 
 interface ModalTypes {
@@ -23,33 +24,36 @@ export function useModal() {
   const state = useAtomValue(modalAtom);
   const setState = useSetAtom(modalAtom);
 
-  const openModal = ({
-    view,
-    title,
-    description,
-    className,
-  }: {
-    view: React.ReactNode;
-    title?: React.ReactNode;
-    description?: React.ReactNode;
-    className?: string;
-  }) => {
-    setState({
-      ...state,
-      isOpen: true,
+  const openModal = useCallback(
+    ({
       view,
       title,
       description,
       className,
-    });
-  };
+    }: {
+      view: React.ReactNode;
+      title?: React.ReactNode;
+      description?: React.ReactNode;
+      className?: string;
+    }) => {
+      setState({
+        ...state,
+        isOpen: true,
+        view,
+        title,
+        description,
+        className,
+      });
+    },
+    [],
+  );
 
-  const closeModal = () => {
+  const closeModal = useCallback(() => {
     setState({
       ...state,
       isOpen: false,
     });
-  };
+  }, []);
 
   return {
     ...state,
