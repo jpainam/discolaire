47249023c90fce86df767aa4882678ab7f27f88c
Jean-Paul Ainam <jpainam@gmail.com>
2024-11-19 15:07:52 -0500

    Add API route for generating classroom PDF reports and implement ClassroomDetails component

---
+++ apps/frontend/src/app/api/pdfs/classroom/route.ts
@@ -0,0 +1,61 @@
+import type { NextRequest } from "next/server";
+import { z } from "zod";
+
+import { ClassroomStudentList, renderToStream } from "@repo/reports";
+
+import { api } from "~/trpc/server";
+
+const searchSchema = z.object({
+  preview: z.coerce.boolean().default(true),
+  size: z.union([z.literal("letter"), z.literal("a4")]).default("letter"),
+  id: z.string().min(1),
+  format: z.union([z.literal("pdf"), z.literal("csv")]).default("pdf"),
+});
+export async function GET(req: NextRequest) {
+  try {
+    const requestUrl = new URL(req.url);
+
+    const obj: Record<string, string> = {};
+
+    for (const [key, value] of requestUrl.searchParams.entries()) {
+      obj[key] = value;
+    }
+
+    const result = searchSchema.safeParse(obj);
+    if (!result.success) {
+      const error = result.error.issues.map((e) => e.message).join(", ");
+      return new Response(error, { status: 400 });
+    }
+    const { id, preview } = result.data;
+
+    const students = await api.classroom.students(id);
+    const classroom = await api.classroom.get(id);
+    const school = await api.school.getSchool();
+
+    const stream = await renderToStream(
+      await ClassroomStudentList({
+        students: students,
+        school: school,
+        size: "a4",
+        classroom: classroom,
+      }),
+    );
+
+    //const blob = await new Response(stream).blob();
+    const headers: Record<string, string> = {
+      "Content-Type": "application/pdf",
+      "Cache-Control": "no-store, max-age=0",
+    };
+    const filename = crypto.randomUUID();
+    if (!preview) {
+      headers["Content-Disposition"] =
+        `attachment; filename="Liste-${filename}.pdf"`;
+    }
+
+    // @ts-expect-error TODO: fix this
+    return new Response(stream, { headers });
+  } catch (error) {
+    console.error(error);
+    return new Response(String(error), { status: 500 });
+  }
+}
---
+++ packages/reports/src/classroom/ClassroomDetails.tsx
@@ -0,0 +1,40 @@
+import { Document, Page, Text, View } from "@react-pdf/renderer";
+
+import type { RouterOutputs } from "@repo/api";
+import { getServerTranslations } from "@repo/i18n/server";
+
+export async function ClassroomDetails({
+  school,
+  classroom,
+}: {
+  school: RouterOutputs["school"]["getSchool"];
+  classroom: RouterOutputs["classroom"]["get"];
+}) {
+  const { t } = await getServerTranslations();
+  return (
+    <Document>
+      <Page
+        style={{
+          padding: 20,
+          fontSize: 9,
+          backgroundColor: "#fff",
+          fontFamily: "Helvetica",
+          color: "#000",
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "column",
+            gap: 10,
+          }}
+        >
+          <View style={{ flexDirection: "row" }}>
+            <Text>{t("name")}</Text>
+            <Text>{classroom.name}</Text>
+            <Text>{school.name}</Text>
+          </View>
+        </View>
+      </Page>
+    </Document>
+  );
+}
