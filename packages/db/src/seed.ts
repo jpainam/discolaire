ce656093de6d45bffee7b640914f00189cd70f5d
Jean-Paul Ainam <jpainam@gmail.com>
2025-02-23 16:23:54 -0500

    feat: add initial database configuration and models for transactions and schedules

---
+++ packages/db/src/seed.ts
@@ -0,0 +1,18 @@
+// import { db } from ".";
+
+// // npx prisma db seed --preview-feature
+// function main() {
+//   console.log("Seeding database...");
+//   return new Promise((resolve, _reject) => {
+//     resolve(true);
+//   });
+// }
+
+// main()
+//   .catch((e) => {
+//     console.error(e);
+//     throw e;
+//   })
+//   .finally(async () => {
+//     await db.$disconnect();
+//   });
