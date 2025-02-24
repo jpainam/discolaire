cf1c8fe538d32819b3ee41ae489c637d660ca574
Jean-Paul Ainam <jpainam@gmail.com>
2025-02-23 22:16:56 -0500

    adding envs

--- packages/auth/src/env.ts
+++ packages/auth/src/env.ts
@@ -3,29 +3,17 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    AUTH_DISCORD_ID: z.string().min(1),
-    AUTH_DISCORD_SECRET: z.string().min(1),
     AUTH_SECRET:
       process.env.NODE_ENV === "production"
         ? z.string().min(1)
         : z.string().min(1).optional(),
     NODE_ENV: z.enum(["development", "production"]).optional(),
-    AUTH_GOOGLE_ID: z.string(),
-    AUTH_GOOGLE_SECRET: z.string(),
-    POSTGRES_URL: z.string(),
-    INVITATION_LINK_SECRET: z.string(),
   },
   client: {},
   runtimeEnv: {
     ...process.env,
-    POSTGRES_URL: process.env.POSTGRES_URL,
     NODE_ENV: process.env.NODE_ENV,
-    INVITATION_LINK_SECRET: process.env.INVITATION_LINK_SECRET,
-    AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
-    AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
     AUTH_SECRET: process.env.AUTH_SECRET,
-    AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
-    AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
   },
   //experimental__runtimeEnv: {},
   skipValidation:
