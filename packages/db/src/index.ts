e8bef79ccebd0e8f9c61930aa9ba791223d663b7
Jean-Paul Ainam <jpainam@gmail.com>
2025-03-02 19:22:44 -0500

    refactor: simplify Prisma client initialization and remove commented code

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,19 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 
-const createPrismaClient = () => new PrismaClient();
-//   {
-//   log:
-//     process.env.NODE_ENV === "development"
-//       ? ["query", "error", "warn"]
-//       : ["error"],
-// }
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
-};
-
-export const db = globalForPrisma.prisma ?? createPrismaClient();
-
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+export const db = globalForPrisma.prisma ?? new PrismaClient();
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+// const createPrismaClient = () => new PrismaClient();
+// //   {
+// //   log:
+// //     process.env.NODE_ENV === "development"
+// //       ? ["query", "error", "warn"]
+// //       : ["error"],
+// // }
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: ReturnType<typeof createPrismaClient> | undefined;
+// };
+
+// export const db = globalForPrisma.prisma ?? createPrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
 
 export * from "@prisma/client";
