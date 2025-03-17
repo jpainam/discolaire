d4445e6bc4a00b83df581fb4218756abd732473a
Jean-Paul Ainam <jpainam@gmail.com>
2025-03-16 20:30:39 -0400

    fix: add missing commas for improved code consistency in ReportCardHeader

--- apps/frontend/src/components/students/report-cards/ReportCardHeader.tsx
+++ apps/frontend/src/components/students/report-cards/ReportCardHeader.tsx
@@ -37,7 +37,7 @@ export function ReportCardHeader({
   const Icon = sidebarIcons.report_cards;
   const canPrintReportCard = useCheckPermission(
     "reportcard",
-    PermissionAction.CREATE
+    PermissionAction.CREATE,
   );
   return (
     <div className="grid md:flex flex-row items-center gap-2 border-b bg-secondary px-4 py-1 text-secondary-foreground">
@@ -81,7 +81,7 @@ export function ReportCardHeader({
                 onSelect={() => {
                   window.open(
                     `/api/pdfs/report-cards/ipbw/?studentId=${params.id}&termId=${searchParams.get("term")}`,
-                    "_blank"
+                    "_blank",
                   );
                 }}
               >
