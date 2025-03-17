412c3970dd58e23644d298eaf1e23853407f1d6b
Jean-Paul Ainam <jpainam@gmail.com>
2025-03-16 20:30:13 -0400

    fix: update layout in ReportCardHeader for improved responsiveness

--- apps/frontend/src/components/students/report-cards/ReportCardHeader.tsx
+++ apps/frontend/src/components/students/report-cards/ReportCardHeader.tsx
@@ -40,7 +40,7 @@ export function ReportCardHeader({
     PermissionAction.CREATE
   );
   return (
-    <div className="flex flex-row items-center gap-2 border-b bg-secondary px-4 py-1 text-secondary-foreground">
+    <div className="grid md:flex flex-row items-center gap-2 border-b bg-secondary px-4 py-1 text-secondary-foreground">
       {Icon && <Icon className="h-6 w-6" />}
       <Label>{t("term")}</Label>
       <TermSelector
