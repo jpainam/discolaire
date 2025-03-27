3d4b33e05a3ca2b33ca5febe5b6c9907e933b580
Jean-Paul Ainam <jpainam@gmail.com>
2025-03-27 18:36:19 -0400

    feat: update Sentry configuration to enable in production environment and modify development script

--- apps/frontend/sentry.edge.config.ts
+++ apps/frontend/sentry.edge.config.ts
@@ -8,6 +8,7 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://e98be2b656e9a50e3fdfcbd540ddc6bb@o4505290610376704.ingest.us.sentry.io/4509051420737536",
 
+  enabled: process.env.NODE_ENV === "production",
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
