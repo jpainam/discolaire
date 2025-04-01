9c3b3646efce15e5aebf50cced83383be57abbac
Jean-Paul Ainam <jpainam@gmail.com>
2025-04-01 05:55:03 -0400

    fix: correct syntax by adding missing commas in CreateEditAssignment and EnrollmentHeader components

--- apps/frontend/src/components/classrooms/assignments/CreateEditAssignment.tsx
+++ apps/frontend/src/components/classrooms/assignments/CreateEditAssignment.tsx
@@ -438,8 +438,8 @@ export function CreateEditAssignment({
                                     ])
                                   : field.onChange(
                                       field.value?.filter(
-                                        (value) => value !== item.value
-                                      )
+                                        (value) => value !== item.value,
+                                      ),
                                     );
                               }}
                             />
