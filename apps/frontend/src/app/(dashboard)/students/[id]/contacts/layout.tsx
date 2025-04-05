166c37590a2a007349d994fcf76afb2ce08afb56
Jean-Paul Ainam <jpainam@gmail.com>
2025-04-05 16:36:57 -0400

    feat: add loading skeleton for StudentContactTable component

--- apps/frontend/src/app/(dashboard)/students/[id]/contacts/layout.tsx
+++ apps/frontend/src/app/(dashboard)/students/[id]/contacts/layout.tsx
@@ -1,5 +1,7 @@
 import { Separator } from "@repo/ui/components/separator";
+import { Skeleton } from "@repo/ui/components/skeleton";
 import type { Metadata } from "next";
+import { Suspense } from "react";
 
 import { SignUpContact } from "~/components/students/contacts/SignUpContact";
 import { StudentContactHeader } from "~/components/students/contacts/StudentContactHeader";
@@ -25,11 +27,23 @@ export default async function Layout(props: {
       <StudentContactHeader />
       <Separator />
       <SignUpContact />
-      <StudentContactTable
-        //studentContacts={studentContacts}
-        studentId={params.id}
-      />
+      <Suspense fallback={<StudentContactSkeleton />}>
+        <StudentContactTable
+          //studentContacts={studentContacts}
+          studentId={params.id}
+        />
+      </Suspense>
       {children}
+    </div>
+  );
+}
+
+function StudentContactSkeleton() {
+  return (
+    <div className="grid grid-cols-6 gap-4 w-full p-4">
+      {Array.from({ length: 12 }).map((_, index) => (
+        <Skeleton key={index} className="h-8" />
+      ))}
     </div>
   );
 }
