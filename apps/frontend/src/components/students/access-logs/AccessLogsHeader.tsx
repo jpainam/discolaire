561da7cfa58be1d9e432e35d9c0f20c1013a9986
Jean-Paul Ainam <jpainam@gmail.com>
2025-04-15 13:01:19 -0400

    fix: add missing commas in UserAccessLogHeader and AccessLogsHeader components

--- apps/frontend/src/components/students/access-logs/AccessLogsHeader.tsx
+++ apps/frontend/src/components/students/access-logs/AccessLogsHeader.tsx
@@ -35,7 +35,7 @@ export function AccessLogsHeader({ userId }: { userId: string }) {
     trpc.loginActivity.delete.mutationOptions({
       onSuccess: async () => {
         await queryClient.invalidateQueries(
-          trpc.loginActivity.all.pathFilter()
+          trpc.loginActivity.all.pathFilter(),
         );
         toast.success(t("deleted_successfully"), { id: 0 });
         router.refresh();
@@ -43,7 +43,7 @@ export function AccessLogsHeader({ userId }: { userId: string }) {
       onError: (error) => {
         toast.error(error.message, { id: 0 });
       },
-    })
+    }),
   );
   return (
     <div className="flex flex-row gap-2 border-b items-center bg-secondary px-4 py-1 text-secondary-foreground">
@@ -63,7 +63,7 @@ export function AccessLogsHeader({ userId }: { userId: string }) {
               onSelect={() => {
                 window.open(
                   `/api/accesslogs?format=pdf&userId=${userId}`,
-                  "_blank"
+                  "_blank",
                 );
               }}
             >
@@ -74,7 +74,7 @@ export function AccessLogsHeader({ userId }: { userId: string }) {
               onSelect={() => {
                 window.open(
                   `/api/accesslogs?format=csv&userId=${userId}`,
-                  "_blank"
+                  "_blank",
                 );
               }}
             >
