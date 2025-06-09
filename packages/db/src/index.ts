1cf7d2a67ba6da1b9252fee40eda6538b0668c51
Jean-Paul Ainam <jpainam@gmail.com>
2025-06-08 18:01:01 -0700

    refactor: clean up Prisma client initialization and configuration in schema.prisma and index.ts

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,22 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
 export const db = globalForPrisma.prisma ?? new PrismaClient();
+
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
-// const createPrismaClient = () => new PrismaClient();
-// //   {
-// //   log:
-// //     process.env.NODE_ENV === "development"
-// //       ? ["query", "error", "warn"]
-// //       : ["error"],
-// // }
-
-// const globalForPrisma = globalThis as unknown as {
-//   prisma: ReturnType<typeof createPrismaClient> | undefined;
-// };
-
-// export const db = globalForPrisma.prisma ?? createPrismaClient();
-
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
 
 export * from "@prisma/client";
