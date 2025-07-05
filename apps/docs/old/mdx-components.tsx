f5280df45883605f98edce7a038d0da0bfbfa036
Jean-Paul Ainam <jpainam@gmail.com>
2025-07-04 19:27:28 -0700

    Remove obsolete images and configuration files; update TypeScript and Docker configurations

--- apps/docs/old/mdx-components.tsx
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-import { useMDXComponents as getDocsMDXComponents } from "nextra-theme-docs";
-
-const docsComponents = getDocsMDXComponents();
-
-export const useMDXComponents = (components) => ({
-  ...docsComponents,
-  ...components,
-});
--- apps/docs/src/app/_meta.js
+++ apps/docs/src/app/_meta.js
@@ -2,16 +2,4 @@ export default {
   index: {
     display: "hidden",
   },
-  docs: {
-    type: "page",
-    title: "Documentation",
-  },
-  pricing: {
-    type: "page",
-    title: "Pricing",
-  },
-  blog: {
-    type: "page",
-    title: "Blog",
-  },
 };
