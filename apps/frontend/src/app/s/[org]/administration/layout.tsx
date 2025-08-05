08550c4d0894f05c512ccc038ea2f9f555fd276a
Jean-Paul Ainam <jpainam@gmail.com>
2025-08-05 08:29:30 -0700

    refactor: convert Layout component to async and restore permission check logic

--- apps/frontend/src/app/s/[org]/administration/layout.tsx
+++ apps/frontend/src/app/s/[org]/administration/layout.tsx
@@ -1,16 +1,19 @@
 import type { PropsWithChildren } from "react";
+import { redirect } from "next/navigation";
 
+import { PermissionAction } from "~/permissions";
+import { checkPermission } from "~/permissions/server";
 import { UpdateAdminBreadcrumb } from "./UpdateAdminBreadcrumb";
 
-export default function Layout({ children }: PropsWithChildren) {
+export default async function Layout({ children }: PropsWithChildren) {
   //const session = await getSession();
-  // const canSeeAdminMenu = await checkPermission(
-  //   "menu:administration",
-  //   PermissionAction.READ,
-  // );
-  // if (!canSeeAdminMenu) {
-  //   redirect("/");
-  // }
+  const canSeeAdminMenu = await checkPermission(
+    "menu:administration",
+    PermissionAction.READ,
+  );
+  if (!canSeeAdminMenu) {
+    redirect("/");
+  }
   return (
     <div className="flex flex-col">
       <UpdateAdminBreadcrumb />
