d10f5afb2e71884a60cdcc75abc6f2b9d88ae256
Jean-Paul Ainam <jpainam@gmail.com>
2025-08-05 07:57:24 -0700

    refactor: remove unused permission checks and redirect logic from layout component

--- apps/frontend/src/app/s/[org]/administration/layout.tsx
+++ apps/frontend/src/app/s/[org]/administration/layout.tsx
@@ -1,19 +1,16 @@
 import type { PropsWithChildren } from "react";
-import { redirect } from "next/navigation";
 
-import { PermissionAction } from "~/permissions";
-import { checkPermission } from "~/permissions/server";
 import { UpdateAdminBreadcrumb } from "./UpdateAdminBreadcrumb";
 
-export default async function Layout({ children }: PropsWithChildren) {
+export default function Layout({ children }: PropsWithChildren) {
   //const session = await getSession();
-  const canSeeAdminMenu = await checkPermission(
-    "menu:administration",
-    PermissionAction.READ,
-  );
-  if (!canSeeAdminMenu) {
-    redirect("/");
-  }
+  // const canSeeAdminMenu = await checkPermission(
+  //   "menu:administration",
+  //   PermissionAction.READ,
+  // );
+  // if (!canSeeAdminMenu) {
+  //   redirect("/");
+  // }
   return (
     <div className="flex flex-col">
       <UpdateAdminBreadcrumb />
