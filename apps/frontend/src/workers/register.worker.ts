7d746711c3e8f61bd72a15138c58d965a092f7ca
Jean-Paul Ainam <jpainam@gmail.com>
2025-08-05 20:12:48 -0700

    fix: remove unused register worker file

--- apps/frontend/src/workers/register.worker.ts
+++
@@ -1,5 +0,0 @@
-// // All jobs need to be exported for the worker to pick them up
-// export * from "./db-backup.worker";
-// export * from "./grade-notification.worker";
-// export * from "./log.worker";
-// export * from "./transaction-summary.worker";
