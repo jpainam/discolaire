40694e1f44e153152c96501fb1279b92ec3d84ca
Jean-Paul Ainam <jpainam@gmail.com>
2025-09-10 22:09:58 -0700

    Using docker

---
+++ apps/frontend/src/app/_error.tsx
@@ -0,0 +1,11 @@
+
+import type { NextPageContext } from "next";
+
+function ErrorPage({ statusCode }: { statusCode?: number }) {
+  return <p>Error {statusCode ?? ""}</p>;
+}
+ErrorPage.getInitialProps = ({ res, err }: NextPageContext) => {
+  const statusCode = res?.statusCode ?? err?.statusCode ?? 404;
+  return { statusCode };
+};
+export default ErrorPage;
