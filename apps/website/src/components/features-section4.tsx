389cc0d91ed6004ba6d24af46648cdbdf61c5f6e
Jean-Paul Ainam <jpainam@gmail.com>
2025-10-10 00:20:40 -0700

    update created grade

--- apps/website/src/components/features-section4.tsx
+++ apps/website/src/components/features-section4.tsx
@@ -33,7 +33,7 @@ export function FeaturesSection4() {
           </div>
         ))}
       </div>
-      <div className="mt-2 mt-10 flex justify-center py-5 text-center text-lg text-neutral-600 dark:text-neutral-400">
+      <div className="mt-10 mt-2 flex justify-center py-5 text-center text-lg text-neutral-600 dark:text-neutral-400">
         <p>
           Want to learn more? Dive into our extensive collection of guides
           covering a wide array of topics, providing in-depth insights and{" "}
@@ -78,8 +78,8 @@ export const Grid = ({
     [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
   ];
   return (
-    <div className="pointer-events-none absolute top-0 left-1/2 -mt-2 -ml-20 h-full w-full [mask-image:linear-gradient(white,transparent)]">
-      <div className="absolute inset-0 bg-gradient-to-r from-zinc-100/30 to-zinc-300/30 [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] opacity-100 dark:from-zinc-900/30 dark:to-zinc-900/30">
+    <div className="pointer-events-none absolute left-1/2 top-0 -ml-20 -mt-2 h-full w-full [mask-image:linear-gradient(white,transparent)]">
+      <div className="absolute inset-0 bg-gradient-to-r from-zinc-100/30 to-zinc-300/30 opacity-100 [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-zinc-900/30 dark:to-zinc-900/30">
         <GridPattern
           width={size ?? 20}
           height={size ?? 20}
