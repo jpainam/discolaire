d23ddd5d5af6db14433a77e5003089723542369d
Jean-Paul Ainam <jpainam@gmail.com>
2025-10-20 11:38:37 -0700

    remove jean -marie

--- apps/frontend/src/actions/sync_attendance.ts
+++ apps/frontend/src/actions/sync_attendance.ts
@@ -5,6 +5,7 @@
  * 119: 535a9326-5f5e-4fa6-a58b-f87decf716de, user: c738c503-73a3-40e8-8d31-f9bfdac7e6c0
  * 208: fa23005a-fbda-4c41-bbc3-f42a1a4d9e01, user: a41bb4e0-be69-4623-86f0-cd761bed25d9
  * 154: db43931b-3ac9-4530-8a16-1af09e178aa5, user: 99872187-34d6-4a0c-bad9-e8413cf935a9
+ * admin: http://192.168.1.112:3000/users/04c268f7-81c4-436a-905f-7b2a6462e67c
  */
 import { AttendanceType } from "@repo/db/enums";
 
@@ -14,7 +15,8 @@ import { studentIdMap } from "./student_id_map";
 
 const staff_map: Record<string, string> = {
   "119": "c738c503-73a3-40e8-8d31-f9bfdac7e6c0",
-  "208": "a41bb4e0-be69-4623-86f0-cd761bed25d9",
+  //"208": "a41bb4e0-be69-4623-86f0-cd761bed25d9",
+  "208": "04c268f7-81c4-436a-905f-7b2a6462e67c",
   "154": "99872187-34d6-4a0c-bad9-e8413cf935a9",
 };
 
