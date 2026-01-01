08fc0bdebd95bdb5328eb77db53b0b1a0224ae35
Jean-Paul Ainam <jpainam@gmail.com>
2026-01-01 10:11:25 -0800

    updating create transaction

--- apps/frontend/src/app/(dashboard)/students/[id]/transactions/create/page.tsx
+++ apps/frontend/src/app/(dashboard)/students/[id]/transactions/create/page.tsx
@@ -6,7 +6,7 @@ import { EmptyComponent } from "~/components/EmptyComponent";
 import { ErrorFallback } from "~/components/error-fallback";
 import { Step1 } from "~/components/students/transactions/create/step1";
 import { Step2 } from "~/components/students/transactions/create/step2";
-import { caller } from "~/trpc/server";
+import { getQueryClient, trpc } from "~/trpc/server";
 import { createTransactionLoader } from "~/utils/search-params";
 
 interface PageProps {
@@ -17,18 +17,33 @@ interface PageProps {
 export default async function Page(props: PageProps) {
   const params = await props.params;
   const t = await getTranslations();
+  const queryClient = getQueryClient();
 
   const searchParams = await createTransactionLoader(props.searchParams);
-  const unpaidRequiredFees = await caller.student.unpaidRequiredFees(params.id);
-  const classroom = await caller.student.classroom({ studentId: params.id });
-  const student = await caller.student.get(params.id);
-  const transactions = await caller.student.transactions(params.id);
+  const unpaidRequiredFees = await queryClient.fetchQuery(
+    trpc.student.unpaidRequiredFees.queryOptions(params.id),
+  );
+  const classroom = await queryClient.fetchQuery(
+    trpc.student.classroom.queryOptions({ studentId: params.id }),
+  );
+  const student = await queryClient.fetchQuery(
+    trpc.student.get.queryOptions(params.id),
+  );
+  const transactions = await queryClient.fetchQuery(
+    trpc.student.transactions.queryOptions(params.id),
+  );
   if (!classroom) {
     return <EmptyComponent title={t("student_not_registered_yet")} />;
   }
-  let fees = await caller.classroom.fees(classroom.id);
-  const studentContacts = await caller.student.contacts(params.id);
-  const journals = await caller.accountingJournal.all();
+  let fees = await queryClient.fetchQuery(
+    trpc.classroom.fees.queryOptions(classroom.id),
+  );
+  const studentContacts = await queryClient.fetchQuery(
+    trpc.student.contacts.queryOptions(params.id),
+  );
+  const journals = await queryClient.fetchQuery(
+    trpc.accountingJournal.all.queryOptions(),
+  );
   if (unpaidRequiredFees.unpaid == 0) {
     fees = fees.filter(
       (j) =>
