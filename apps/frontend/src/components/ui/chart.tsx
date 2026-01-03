5eb105f468ae9db99e588bfba5a3d955ceeb32bb
Jean-Paul Ainam <jpainam@gmail.com>
2026-01-02 23:12:59 -0800

    update schema

--- apps/frontend/src/components/ui/chart.tsx
+++ apps/frontend/src/components/ui/chart.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unnecessary-condition */
+
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
+
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
 "use client";
