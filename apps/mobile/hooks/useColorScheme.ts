f3bbdaf4496bd2ea688f475b4167c816940b80bb
Jean-Paul Ainam <jpainam@gmail.com>
2026-02-14 21:40:45 -0800

    adding native from hero ui

--- apps/mobile/hooks/useColorScheme.ts
+++
@@ -1,9 +0,0 @@
-import { useColorScheme as useColorSchemeRoot } from "react-native";
-
-export function useColorScheme(): "light" | "dark" {
-  const theme = useColorSchemeRoot();
-  if (theme === "unspecified") {
-    return "light";
-  }
-  return theme;
-}
--- apps/mobile/stores/persistor.ts
+++
@@ -1,6 +0,0 @@
-// import AsyncStorage from "@react-native-async-storage/async-storage";
-// import { createAsyncStoragePersistor } from "@tanstack/react-query-persist-client-async-storage";
-
-// export const asyncStoragePersistor = createAsyncStoragePersistor({
-//   storage: AsyncStorage,
-// });
